538e6f992f2a55962b98323a2a9e1db9d77c3818
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-10 23:47:59 +0530

    Some Chages

--- src/maps/AddressPage.js
+++ src/maps/AddressPage.js
@@ -8,23 +8,18 @@ const CurrentAddress = () => {
     useEffect(() => {
       const fetchData = async () => {
         try {
-          // Get the current position using the browser's geolocation
           const position = await new Promise((resolve, reject) => {
             navigator.geolocation.getCurrentPosition(resolve, reject);
           });
   
-          // Retrieve latitude and longitude from the position object
           const { latitude, longitude } = position.coords;
   
-          // Make a request to the OpenCageData API
           const response = await axios.get(
             `https://api.opencagedata.com/geocode/v1/json?key=48470a7625c44368a8544c93e27376ea&q=${latitude},${longitude}`
           );
   
-          // Extract the formatted address from the API response
           const formattedAddress = response.data.results[0].formatted;
   
-          // Update the address state
           setAddress(formattedAddress);
         } catch (error) {
           console.log('Error occurred while retrieving address:', error);
