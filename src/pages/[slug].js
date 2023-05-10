ed27f462c4cb5e636499435ee76fa94d1e24fb46
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-10 23:48:43 +0530

    Some chage in slug

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function RTLT() {
   return (
-    <section className="flex items-center min-h-screen sm:p-16 dark:bg-gray-900 dark:text-gray-100">
+    <section className="flex items-center min-h-screen sm:p-16">
 	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-40 h-40 dark:text-gray-600">
 			<path fill="currentColor" d="M256,16C123.452,16,16,123.452,16,256S123.452,496,256,496,496,388.548,496,256,388.548,16,256,16ZM403.078,403.078a207.253,207.253,0,1,1,44.589-66.125A207.332,207.332,0,0,1,403.078,403.078Z"></path>
